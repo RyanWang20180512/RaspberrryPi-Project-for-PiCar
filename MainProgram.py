e7640e6c49de3c5473fa60bbadb772acdfb345c6
Wang Ryan <wzy115101000180@outlook.com>
2018-07-18 12:09:08 +0000

    Combine the motor control and camera control in one thread. Write a program to send frames in UDP. Now the problem is the main program cost too much cpu

--- MainProgram.py
+++ MainProgram.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python
 import socket
 from enum import IntEnum
 from MotorModule.Motor import Motor
@@ -5,6 +6,8 @@ import traceback
 import threading
 import time
 from SteeringModule.Steering import Steering
+import cv2
+import numpy
 
 def getLocalIp():
     '''Get the local ip'''
@@ -15,6 +18,17 @@ def getLocalIp():
     finally:
         s.close()
     return ip
+
+def cameraAction(steer,command):
+    if command=='CamUp':
+        steer.Up()
+    elif command=='CamDown':
+        steer.Down()
+    elif command=='CamLeft':
+        steer.Left()
+    elif command=='CamRight':
+        steer.Right()
+
 
 def motorAction(motor,command):
     '''Set the action of motor according to the command'''
@@ -32,6 +46,10 @@ def motorAction(motor,command):
 def setCameraAction(command):
     if command=='CamUp' or command=='CamDown' or command=='CamLeft' or command=='CamRight':
         return command
+    else:
+        return 'CamStop'
+
+            
 
 def cameraActionThread():
     '''This is a child thread to control the action of camera'''
@@ -73,18 +91,20 @@ def main():
     motor.setup()
 
     #Set a state variable for steering module
-    global cameraAction
-    cameraAction='CamStop'
+    global cameraActionState
+    cameraActionState='CamStop'
     global stopCameraThread
     stopCameraThread=False
-
     while True:
         try:
             (client,addr)=tcpServer.accept()
             print('accept the client!')
-            stopCameraThread=False
-            camera_action_thread=threading.Thread(target=cameraActionThread,args=())
-            camera_action_thread.start()
+            client.setblocking(0)
+            #stopCameraThread=False
+            #camera_action_thread=threading.Thread(target=cameraActionThread,args=())
+            #camera_action_thread.start()
+            steer=Steering(14,0,180,15,90,180,36,160)
+            steer.setup()
             while True:
                 try:
                     data=client.recv(1024)
@@ -93,22 +113,25 @@ def main():
                         print('client is closed')
                         break
                     motorAction(motor,data)
-                    cameraAction=setCameraAction(data)
+                    cameraActionState=setCameraAction(data)
+                    cameraAction(steer,cameraActionState)           
                 except socket.error:
+                    cameraAction(steer,cameraActionState)
                     pass
-                except Exception:
-                    raise Exception
+                #except Exception:
+                    #raise Exception
             stopCameraThread=True #Notice the camera action thread to stop
-            time.sleep(1)
         except socket.error:
             pass
         except Exception,e1:
             traceback.print_exc()
             stopCameraThread=True
             motor.clear()
+            steer.cleanup()
             tcpServer.close()
-    stopCameraThread=True
-    motor.clear()
-    tcpServer.close()
-
+        #finally:
+            #stopCameraThread=True
+            #motor.clear()
+            #tcpServer.close()
+            #steer.cleanup()
 main()
---
+++ VideoFramesSendService.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import cv2
+import numpy
+import socket
+import time
+import struct
+
+#HOST='192.168.191.1'
+HOST='255.255.255.255'
+PORT=5051
+WIDTH=320
+HEIGHT=240
+
+server=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+server.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1) #enable broadcast
+server.connect((HOST,PORT))
+print('now starting to send frames...')
+capture=cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH,WIDTH)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT,HEIGHT)
+try:
+    while True:
+        #time.sleep(0.5)
+        success,frame=capture.read()
+        while not success and frame is None:
+            success,frame=capture.read()
+        result,imgencode=cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,30])
+        #result,imgencode=cv2.imencode('.webp',frame,[cv2.IMWRITE_WEBP_QUALITY,20])
+        #print(len(imgencode))
+	server.sendall(imgencode)
+#	print('have sent one frame')
+except Exception as e:
+    server.sendall(struct.pack('b',1))
+    print(e)
+    capture.release()
+    server.close()
+    
